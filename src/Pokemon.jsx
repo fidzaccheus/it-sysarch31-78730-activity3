82058689d29273d3a15ad9e39cf17434d9fb5a0e
fidzaccheus <fidzaccheus@gmail.com>
2024-03-22 22:10:35 +0800

    finished styling the webpage

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,19 +1,28 @@
 const Pokemon = ({ pokemon, language }) => {
   return (
     <li className="pokemonContainer">
-      <img src={pokemon.image} alt={pokemon.name.english} />
-      <ul className="pokemonData">
-        <li>ID: {pokemon.id}</li>
-        <li>Name: {pokemon.name[language]}</li>
+      <ul>
         <li>
-          <ul className="type">
+          <img loading="lazy" src={pokemon.image} alt={pokemon.name.english} />
+        </li>
+        <li className="pokemonName">
+          [{pokemon.id}] {pokemon.name[language]}
+        </li>
+        <li>
+          <ul className="pokemonType">
             {pokemon.type.map((type, index) => (
               <li key={index}>{type}</li>
             ))}
           </ul>
         </li>
-        <li>HP: {pokemon.base.HP}</li>
-        <li>Attack: {pokemon.base.Attack}</li>
+        <ul className="columned">
+          <li>HP: {pokemon.base.HP}</li>
+          <li>Attk: {pokemon.base.Attack}</li>
+          <li>Def: {pokemon.base.Defense}</li>
+          <li>Speed: {pokemon.base.Speed}</li>
+          <li>Sp. Attack: {pokemon.base["Sp. Attack"]}</li>
+          <li>Sp. Defense: {pokemon.base["Sp. Defense"]}</li>
+        </ul>
       </ul>
     </li>
   );
